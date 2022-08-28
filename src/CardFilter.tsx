5a0086466b402a66bd76ea7fede67f169316ff6a
a_kiritani <a.kiritani@skillengine.jp>
2022-08-28 23:41:36 +0900

    カードフィルターの修正

--- src/CardFilter.tsx
+++ src/CardFilter.tsx
@@ -3,7 +3,13 @@ import styled from 'styled-components'
 import * as color from './color'
 import { SearchIcon as _SearchIcon } from './icon'
 
-export function CardFilter() {
+export function CardFilter({
+    value,
+    onChange,
+  }: {
+    value?: string
+    onChange?(value: string): void
+  }) {
   return (
     <Container>
       <SearchIcon />
